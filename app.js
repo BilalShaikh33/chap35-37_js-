f9c95e96e4c175f20060fc4ad14df797cd9fb373
Bilal Shaikh <bilalshaikhh339@gmail.com>
2025-11-12 06:18:00 -0800

    i

--- app.js
+++ app.js
@@ -14,40 +14,40 @@
 // fact()
 
 
-function calculator() {
-    var num1 = +prompt("Enter your fisrt number")
-    var num2 = +prompt("Enter your second number")
-    var op = prompt("Enter your operator number")
-    var resultnum = output(num1, num2, op)
-      var resultnum = output(num1, num2, op);
-    if (resultnum != null) {
-        console.log("Result:", resultnum);
-    } else {
-        console.log("Invalid operator!");
-    }
+// function calculator() {
+//     var num1 = +prompt("Enter your fisrt number")
+//     var num2 = +prompt("Enter your second number")
+//     var op = prompt("Enter your operator number")
+//     var resultnum = output(num1, num2, op)
+//       var resultnum = output(num1, num2, op);
+//     if (resultnum != null) {
+//         console.log("Result:", resultnum);
+//     } else {
+//         console.log("Invalid operator!");
+//     }
 
 
 
 
-}
+// }
 
-function output(a, b, op) {
-    if (op == "+") {
-        return (a + b)
+// function output(a, b, op) {
+//     if (op == "+") {
+//         return (a + b)
 
-    }
-    if (op == "-") {
-        return (a - b)
+//     }
+//     if (op == "-") {
+//         return (a - b)
 
-    }
-    if (op == "/") {
-        return (a / b)
+//     }
+//     if (op == "/") {
+//         return (a / b)
 
-    }
-    if (op == "*") {
-        return (a * b)
+//     }
+//     if (op == "*") {
+//         return (a * b)
 
-    }
-}
+//     }
+// }
 
-calculator()
+// calculator()
