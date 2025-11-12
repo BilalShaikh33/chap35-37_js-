4e43d90b4ae27d472d2fb7682d3319ed98559f41
Bilal Shaikh <bilalshaikhh339@gmail.com>
2025-11-12 04:06:32 -0800

    i

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
         
         
-        function fact (){
-            let num = +prompt("Enter any number")
+        // function fact (){
+        //     let num = +prompt("Enter any number")
 
-            for (let i = num ; i > 0; i--) {
-                num = num * i
-            }
+        //     for (let i = num ; i > 0; i--) {
+        //         num = num * i
+        //     }
             
-            document.writeln(num)
+        //     document.writeln(num)
         
+        // }
+        
+        // fact()
+
+
+    function calculator(){
+        var num1 = +prompt("Enter your fisrt number")
+        var num2 = +prompt("Enter your second number")
+        var op = +prompt("Enter your operator number")
+        if (num1 == 0 || num1 == "" || num2 == 0 || num2 == "") {
+            document.writeln("Enter correct number")
+
         }
-        
-        fact()
+        // else{
+            
+        }
+
+
+
+    }
         
