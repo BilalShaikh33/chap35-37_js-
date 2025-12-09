ce86db1c428cd3bebbcb28c7430c418c8a62da00
Bilal Shaikh <bilalshaikhh339@gmail.com>
2025-12-09 18:17:10 +0500

    i

--- app.js
+++ app.js
@@ -1,76 +1,76 @@
 
 
-// function fact (){
-//     let num = +prompt("Enter any number")
+function fact (){
+    let num = +prompt("Enter any number")
 
-//     for (let i = num ; i > 0; i--) {
-//         num = num * i
-//     }
+    for (let i = num ; i > 0; i--) {
+        num = num * i
+    }
 
-//     document.writeln(num)
+    document.writeln(num)
 
-// }
+}
 
-// fact()
-
-
-// function calculator() {
-//     var num1 = +prompt("Enter your fisrt number")
-//     var num2 = +prompt("Enter your second number")
-//     var op = prompt("Enter your operator number")
-//     var resultnum = output(num1, num2, op)
-//       var resultnum = output(num1, num2, op);
-//     if (resultnum != null) {
-//         console.log("Result:", resultnum);
-//     } else {
-//         console.log("Invalid operator!");
-//     }
+fact()
 
 
+function calculator() {
+    var num1 = +prompt("Enter your fisrt number")
+    var num2 = +prompt("Enter your second number")
+    var op = prompt("Enter your operator number")
+    var resultnum = output(num1, num2, op)
+      var resultnum = output(num1, num2, op);
+    if (resultnum != null) {
+        console.log("Result:", resultnum);
+    } else {
+        console.log("Invalid operator!");
+    }
 
 
-// }
-
-// function output(a, b, op) {
-//     if (op == "+") {
-//         return (a + b)
-
-//     }
-//     if (op == "-") {
-//         return (a - b)
-
-//     }
-//     if (op == "/") {
-//         return (a / b)
-
-//     }
-//     if (op == "*") {
-//         return (a * b)
-
-//     }
-// }
-
-// calculator()
 
 
-// function square(){
+}
 
-//     num = +prompt("Enter your number")
-//     if (num!= null) {
-//         sq = num * num
+function output(a, b, op) {
+    if (op == "+") {
+        return (a + b)
+
+    }
+    if (op == "-") {
+        return (a - b)
+
+    }
+    if (op == "/") {
+        return (a / b)
+
+    }
+    if (op == "*") {
+        return (a * b)
+
+    }
+}
+
+calculator()
+
+
+function square(){
+
+    num = +prompt("Enter your number")
+    if (num!= null) {
+        sq = num * num
         
-//     }
-//     else{
-//         console.log("Enter a valid number")
+    }
+    else{
+        console.log("Enter a valid number")
 
-//     }
-//     document.writeln(`"sqare of " ${num} " is   ${sq} `)
+    }
+    document.writeln(`"sqare of " ${num} " is   ${sq} `)
 
 
 
-// }
+}
 
-// square()
+square()
 
 
 // function test(counter) {
