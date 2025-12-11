c56a02458e67d5859b297ccc4d6debe8130594b8
Bilal Shaikh <bilalshaikhh339@gmail.com>
2025-12-11 17:34:19 +0500

    i

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 
 
-function fact (){
-    let num = +prompt("Enter any number")
+// function fact (){
+//     let num = +prompt("Enter any number")
 
-    for (let i = num ; i > 0; i--) {
-        num = num * i
-    }
+//     for (let i = num ; i > 0; i--) {
+//         num = num * i
+//     }
 
-    document.writeln(num)
+//     document.writeln(num)
 
-}
+// }
 
 // fact()
 
 
-// function calculator() {
-//     var num1 = +prompt("Enter your fisrt number")
-//     var num2 = +prompt("Enter your second number")
-//     var op = prompt("Enter your operator number")
-//     var resultnum = output(num1, num2, op)
-//       var resultnum = output(num1, num2, op);
-//     if (resultnum != null) {
-//         console.log("Result:", resultnum);
-//     } else {
-//         console.log("Invalid operator!");
-//     }
+function calculator() {
+    var num1 = +prompt("Enter your fisrt number")
+    var num2 = +prompt("Enter your second number")
+    var op = prompt("Enter your operator number")
+    var resultnum = output(num1, num2, op)
+      var resultnum = output(num1, num2, op);
+    if (resultnum != null) {
+        console.log("Result:", resultnum);
+    } else {
+        console.log("Invalid operator!");
+    }
 
 
 
